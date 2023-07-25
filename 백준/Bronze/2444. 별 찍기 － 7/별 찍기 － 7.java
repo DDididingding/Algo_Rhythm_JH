3ebe19ef9d7e1d1609e1ef0dbd2c2b7bdeb67748
JH An <68099714+DDididingding@users.noreply.github.com>
2023-07-25 11:51:26 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 400 ms, Memory: 14788 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -0,0 +1,50 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		int upper = 0;
+		int upper1 = 1;
+		int cnt = 0;
+		int cnt1 = 2*N-1;
+		
+		for (int i = 0; i <N-1; i++) {
+			for (int j = 0; j < N-1-upper; j++ ) {
+				System.out.print(" ");
+				}
+			
+			for (int j = 0; j < upper1; j++) {
+				System.out.print("*");
+			}
+			
+			System.out.println();
+			upper++;
+			upper1 +=2;
+			
+		}
+		
+		
+		for (int i = 0; i<N; i++) { //줄 개수
+			
+			
+			for (int j = 0; j<cnt; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 0; j<cnt1; j ++) {
+				System.out.print("*");
+			}
+			
+			cnt++;
+			cnt1 -= 2;
+			System.out.println();
+		}
+		
+		
+	}
+
+}
