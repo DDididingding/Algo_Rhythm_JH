62b52c6ca70d41efb71c11578bb6d4a568d71c1e
JH An <68099714+DDididingding@users.noreply.github.com>
2023-08-17 15:20:48 +0900

    [D4] Title: [S/W 문제해결 기본] 6일차 - 계산기3, Time: 130 ms, Memory: 20,164 KB -BaekjoonHub

---
+++ SWEA/D4/1224. ［S／W 문제해결 기본］ 6일차 － 계산기3/［S／W 문제해결 기본］ 6일차 － 계산기3.java
@@ -0,0 +1,115 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Solution {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		for(int t = 1; t<=10; t++) {
+			String trash = br.readLine();
+			String[] str = br.readLine().split("");
+			StringBuilder sb = new StringBuilder();
+			Stack<String> stack = new Stack<>();
+			
+			int result= 0;
+			for(int i = 0; i< str.length; i++) {
+				if(str[i].equals("(")) {
+					stack.push(str[i]);
+				}else if(str[i].equals(")")) {
+					while(!stack.peek().equals("(")){
+						sb.append(stack.pop());
+					}
+					stack.pop();
+					
+				}else if(str[i].equals("+")) {
+					if(stack.isEmpty()){
+						stack.push(str[i]);
+					}else if(stack.peek().equals("+")||stack.peek().equals("*")||stack.peek().equals("-")||stack.peek().equals("/")) {
+						sb.append(stack.pop());
+						stack.push(str[i]);
+					}else {
+						stack.push(str[i]);
+					}
+						
+				}else if(str[i].equals("-")) {
+					if(stack.isEmpty()){
+						stack.push(str[i]);
+					}else if(stack.peek().equals("+")||stack.peek().equals("*")||stack.peek().equals("-")||stack.peek().equals("/")) {
+						sb.append(stack.pop());
+						stack.push(str[i]);
+					}else {
+						stack.push(str[i]);
+					}
+						
+				}else if(str[i].equals("*")){
+					if(stack.isEmpty()){
+						stack.push(str[i]);
+					}else if(stack.peek().equals("*")||stack.peek().equals("/")) {
+						sb.append(stack.pop());
+						stack.push(str[i]);
+					}else if(stack.peek().equals("+")||stack.peek().equals("-")){
+						stack.push(str[i]);
+					}else {
+						stack.push(str[i]);
+					}
+				}else if(str[i].equals("/")){
+					if(stack.isEmpty()){
+						stack.push(str[i]);
+					}else if(stack.peek().equals("*")||stack.peek().equals("/")) {
+						sb.append(stack.pop());
+						stack.push(str[i]);
+					}else if(stack.peek().equals("+")||stack.peek().equals("-")){
+						stack.push(str[i]);
+					}else {
+						stack.push(str[i]);
+					}
+				}else {
+					sb.append(str[i]);
+				}
+			}
+			while(!stack.isEmpty()) {
+				sb.append(stack.pop());
+			}//후위 연산자로 바꾸기
+			
+//			System.out.println(sb);
+			
+			Stack<Integer> intStack = new Stack<>();
+			for(int i = 0; i<sb.length(); i++) {
+				if(intStack.isEmpty()) {
+					intStack.push(sb.charAt(i)-'0');
+				}else if(sb.charAt(i) == '+') {
+					int b = intStack.pop();
+					int a = intStack.pop();
+					intStack.push(a+b);
+				}else if(sb.charAt(i) =='*'){
+					int a = intStack.pop();
+					int b = intStack.pop();
+					intStack.push(a*b);
+				}else if(sb.charAt(i) =='-'){
+					int b = intStack.pop();
+					int a = intStack.pop();
+					intStack.push(a-b);
+				}else if(sb.charAt(i) =='/'){
+					int b = intStack.pop();
+					int a = intStack.pop();
+					intStack.push(a/b);
+				}else {
+					intStack.push(sb.charAt(i)-'0');
+				}
+			}
+			
+			while(!intStack.isEmpty()) {
+				result += intStack.pop();
+			}
+			 //후위연산자 계산하기
+			
+			
+			
+			System.out.println("#"+t+" "+result);
+			
+		}
+	}
+
+}
