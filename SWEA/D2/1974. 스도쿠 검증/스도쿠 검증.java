4da94582984ba017c9f61b11783ed90e95f4e7ff
JH An <68099714+DDididingding@users.noreply.github.com>
2023-08-24 15:15:20 +0900

    [D2] Title: 스도쿠 검증, Time: 127 ms, Memory: 18,704 KB -BaekjoonHub

--- SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.java
+++ SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.java
@@ -3,66 +3,80 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Solution {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static int[][] sdoku;
+	static int[] check;
 
-        int T = Integer.parseInt(br.readLine());
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
 
-        for (int t = 1; t <= T; t++) {
-            int[][] sudoku = new int[9][9];
-            boolean good = true;
+		for (int t = 1; t <= T; t++) {
+			sdoku = new int[9][9];
+			boolean isRight = true;
+			for (int i = 0; i < 9; i++) {
+				String[] str = br.readLine().split(" ");
+				for (int j = 0; j < 9; j++) {
+					sdoku[i][j] = Integer.parseInt(str[j]);
+				}
+			}
+			
+			for(int i = 0; i<9; i++) {
+				if(!horCheck(i) || !verCheck(i)) {
+					isRight = false;
+					break;
+				}
+			}
+			if(!boxCheck(0,0) || !boxCheck(0,3) || !boxCheck(0,6) || !boxCheck(3,0) || !boxCheck(3,3) || !boxCheck(3,6) || !boxCheck(6,0) || !boxCheck(6,3) || !boxCheck(6,6) ) {
+				isRight = false;
+			}
 
-            for (int i = 0; i < 9; i++) {
-                String[] str = br.readLine().split(" ");
-                for (int j = 0; j < 9; j++) {
-                    sudoku[i][j] = Integer.parseInt(str[j]);
-                }
-            }
+			if(isRight) {
+				System.out.println("#"+t+" "+1);
+			}else {
+				System.out.println("#"+t+" "+0);
+			}
+		}
 
-            // Check rows and columns
-            for (int i = 0; i < 9; i++) {
-                boolean[] rowCheck = new boolean[10];
-                boolean[] colCheck = new boolean[10];
-                for (int j = 0; j < 9; j++) {
-                    if (rowCheck[sudoku[i][j]] || colCheck[sudoku[j][i]]) {
-                        good = false;
-                        break;
-                    }
-                    rowCheck[sudoku[i][j]] = true;
-                    colCheck[sudoku[j][i]] = true;
-                }
-                if (!good) {
-                    break;
-                }
-            }
+	}
 
-            // Check subgrids
-            for (int i = 0; i < 9; i += 3) {
-                for (int j = 0; j < 9; j += 3) {
-                    boolean[] subgridCheck = new boolean[10];
-                    for (int x = i; x < i + 3; x++) {
-                        for (int y = j; y < j + 3; y++) {
-                            if (subgridCheck[sudoku[x][y]]) {
-                                good = false;
-                                break;
-                            }
-                            subgridCheck[sudoku[x][y]] = true;
-                        }
-                        if (!good) {
-                            break;
-                        }
-                    }
-                }
-                if (!good) {
-                    break;
-                }
-            }
+	static boolean horCheck(int column) {
+		check = new int[10];
 
-            if (good) {
-                System.out.println("#" + t + " " + 1);
-            } else {
-                System.out.println("#" + t + " " + 0);
-            }
-        }
-    }
+		for (int i = 0; i < 9; i++) {
+			check[sdoku[column][i]]++;
+		}
+		for (int i = 1; i <= 9; i++) {
+			if (check[i] != 1)
+				return false;
+		}
+		return true;
+	}
+
+	static boolean verCheck(int row) {
+		check = new int[10];
+
+		for (int i = 0; i < 9; i++) {
+			check[sdoku[i][row]]++;
+		}
+		for (int i = 1; i <= 9; i++) {
+			if (check[i] != 1)
+				return false;
+		}
+		return true;
+	}
+
+	static boolean boxCheck(int y, int x) {
+		check = new int[10];
+		for(int i = y; i<y+3; i++) {
+			for(int j = x; j<x+3; j++) {
+				check[sdoku[i][j]]++;
+			}
+		}
+		for (int i = 1; i <= 9; i++) {
+			if (check[i] != 1)
+				return false;
+		}
+		return true;
+	}
+
 }
