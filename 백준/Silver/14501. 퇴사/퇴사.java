ccd76e84bef0ec8c7260158d5949c9b6550da88d
JH An <68099714+DDididingding@users.noreply.github.com>
2023-09-06 16:29:17 +0900

    [Silver III] Title: 퇴사, Time: 76 ms, Memory: 11628 KB -BaekjoonHub

---
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	static int N;
+	static int[] T;
+	static int[] P;
+	static int maxSum;
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		N = Integer.parseInt(br.readLine());
+		
+		T = new int[N+1];
+		P = new int[N+1];
+		
+		for(int i = 1; i<=N; i++) {
+			String[] str = br.readLine().split(" ");
+			T[i] = Integer.parseInt(str[0]);
+			P[i] = Integer.parseInt(str[1]);
+			
+		}
+		
+		ruJ(1,0);
+		System.out.println(maxSum);
+	}
+	
+	static void ruJ(int day, int sum) {
+		if(day>N) {
+			
+			if(sum>maxSum) maxSum = sum;
+			return;
+		}
+		
+		//골랐으면
+		if(day+T[day]>N+1) {
+			ruJ(day+T[day], sum);
+		}else {
+			ruJ(day+T[day], sum+P[day]);
+		}
+		
+		//안골랐으면
+		ruJ(day+1, sum);
+	}
+
+}
