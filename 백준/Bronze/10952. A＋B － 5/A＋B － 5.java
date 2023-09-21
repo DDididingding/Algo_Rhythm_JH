c76261b72d46837a800bcc1635cd104410c4246a
JH An <68099714+DDididingding@users.noreply.github.com>
2023-09-21 17:04:45 +0900

    [Bronze V] Title: A+B - 5, Time: 84 ms, Memory: 11504 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		while(true) {
+			String st = br.readLine();
+			if(st.equals("0 0")) break;
+			String[] str = st.split(" ");
+			int A = Integer.parseInt(str[0]);
+			int B = Integer.parseInt(str[1]);
+			System.out.println(A+B);
+		}
+	}
+}
